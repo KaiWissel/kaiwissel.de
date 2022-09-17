a1e767c3360ab2828d3424c56e51970d9e36e99b
Kai Wissel <kai.wpkt@gmail.com>
2022-09-18 00:39:45 +0200

    clean up

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((element) => {
-    console.log(element);
     if (element.isIntersecting) {
       element.target.classList.add("show");
       if (element.target.classList.value.includes("logo")) {
-        console.log("logo is visible");
         const stickyNav = document.querySelectorAll(".sticky-nav")[0];
-
-        console.log("SN: ", stickyNav);
         stickyNav.classList.remove("show");
       }
     } else {
       if (element.target.classList.value.includes("logo")) {
-        console.log("logo is not visible");
         const stickyNav = document.querySelectorAll(".sticky-nav")[0];
-
-        console.log("SN: ", stickyNav);
         stickyNav.classList.add("show");
       }
       element.target.classList.remove("show");
