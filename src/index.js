36e7f99a5cf33eec49edb35772a18543b349e7c2
Kai Wissel <kai.wpkt@gmail.com>
2022-09-18 00:10:44 +0200

    sticky-header

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ const observer = new IntersectionObserver((entries) => {
     console.log(element);
     if (element.isIntersecting) {
       element.target.classList.add("show");
+      if (element.target.classList.value.includes("logo")) {
+        console.log("logo is visible");
+        const stickyNav = document.querySelectorAll(".sticky-nav")[0];
+
+        console.log("SN: ", stickyNav);
+        stickyNav.classList.remove("show");
+      }
     } else {
+      if (element.target.classList.value.includes("logo")) {
+        console.log("logo is not visible");
+        const stickyNav = document.querySelectorAll(".sticky-nav")[0];
+
+        console.log("SN: ", stickyNav);
+        stickyNav.classList.add("show");
+      }
       element.target.classList.remove("show");
     }
   });
