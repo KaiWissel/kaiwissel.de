6b9e8166554ce081390251c129f3ec9e777b968b
Kai Wissel <kai.wpkt@gmail.com>
2022-09-18 09:39:18 +0200

    fixed styling errors

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+const SHOW_CLASS = "show";
+const STICKY_ELEMENT = ".sticky-nav";
+const SWITCH_STICKY = "landing-nav";
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((element) => {
     if (element.isIntersecting) {
-      element.target.classList.add("show");
-      if (element.target.classList.value.includes("logo")) {
-        const stickyNav = document.querySelectorAll(".sticky-nav")[0];
-        stickyNav.classList.remove("show");
+      element.target.classList.add(SHOW_CLASS);
+      if (element.target.classList.value.includes(SWITCH_STICKY)) {
+        const stickyNav = document.querySelectorAll(STICKY_ELEMENT)[0];
+        stickyNav.classList.remove(SHOW_CLASS);
       }
     } else {
-      if (element.target.classList.value.includes("logo")) {
-        const stickyNav = document.querySelectorAll(".sticky-nav")[0];
-        stickyNav.classList.add("show");
+      if (element.target.classList.value.includes(SWITCH_STICKY)) {
+        const stickyNav = document.querySelectorAll(STICKY_ELEMENT)[0];
+        stickyNav.classList.add(SHOW_CLASS);
       }
-      element.target.classList.remove("show");
+      element.target.classList.remove(SHOW_CLASS);
     }
   });
 });
